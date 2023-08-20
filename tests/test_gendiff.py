a23ae9332d5b7bf289623d564cb83cedc8492acf
Edhard <41229593+kreker783@users.noreply.github.com>
2023-08-20 19:47:39 +0200

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,13 +4,14 @@ from gendiff.scripts.gendiff import generate_diff
 def test_gendiff():
     first_path = "gendiff/files/file1.json"
     second_path = "gendiff/files/file2.json"
-    result = """{
-                    - follow: false
-                    host: hexlet.io
-                    - proxy: 123.234.53.22
-                    - timeout: 50
-                    + timeout: 20
-                    + verbose: true
-                }"""
+    result = """
+    {
+        - follow: false
+          host: hexlet.io
+        - proxy: 123.234.53.22
+        - timeout: 50
+        + timeout: 20
+        + verbose: true
+    }"""
     assert generate_diff(first_path, second_path) == result
 
