58d8928d4fcb80267436b93c426e36fbc6f9a60a
Ed-Host <kreker783@gmail.com>
2023-09-01 00:07:48 +0200

    Chenge to string instead dict

--- gendiff/logic/converting.py
+++ gendiff/logic/converting.py
@@ -1,32 +1,68 @@
-res = "{"
-temp_dict = {}
+res = "{ \n"
 
 
-def get_line(first_dict, second_dict):
-    get_first_dict(first_dict, second_dict)
+def get_line(first_dict, second_dict, indent=0):
+    get_first_dict(first_dict, second_dict, indent)
 
     for _, (key, item) in enumerate(second_dict.items()):
         if key not in first_dict:
-            add_to_result(key, item, sign='+')
+            if if_dict(item):
+                add_nested_dict(key, item, {}, indent)
+            else:
+                add_to_result(key, item, indent, sign='+')
 
     global res
-    res = res[:-2] + "}"
+    # res += "}"
     return res
 
 
-def get_first_dict(arr1, arr2):
+def get_first_dict(arr1, arr2, indent):
 
     for _, (key, item) in enumerate(arr1.items()):
         if key in arr2 and item == arr2[key]:
-            add_to_result(key, item)
+            if if_dict(item, arr2[key]):
+                add_nested_dict(key, item, arr2[key], indent)
+            else:
+                add_to_result(key, item, indent)
         elif key in arr2:
-            add_to_result(key, item, sign="-")
-            add_to_result(key, arr2[key], sign="+")
+            if if_dict(item, arr2[key]):
+                add_nested_dict(key, item, arr2[key], indent)
+            elif if_dict(item):
+                add_nested_dict(key, item, {}, indent)
+            elif if_dict(arr2[key]):
+                add_nested_dict(key, {}, arr2[key], indent)
+            else:
+                add_to_result(key, item, indent, sign="-")
+                add_to_result(key, arr2[key], indent, sign="+")
         elif key not in arr2:
-            add_to_result(key, item, sign="-")
+            if if_dict(item):
+                add_nested_dict(key, item, {}, indent)
+            else:
+                add_to_result(key, item, indent, sign="-")
 
 
-def add_to_result(key, value, indent=0, sign=" "):
+def if_dict(*args):
+    for item in args:
+        if not isinstance(item, dict):
+            return False
+    return True
+
+
+def add_nested_dict(key, item, item2, indent):
+    add_to_result(key, "{", indent)
+
+    indent += 4
+    get_line(item, item2, indent)
+    indent -= 4
+
+    add_to_result("", "}", indent)
+
+
+def add_to_result(key, value, indent, sign=" "):
     global res
     space = (indent + 2) * " "
-    res += f'"{space}{sign} {key}": {value}, '
+    if value == "}":
+        res += f'{space} {key} {value}\n'
+    else:
+        res += f'{space}{sign} {key}: {value}\n'
+
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -36,13 +36,8 @@ def main():
 
 
 def generate_diff(first_path, second_path):
-
     first_file, second_file = ff.get_dict_from_file(first_path, second_path)
-    x = get_line(first_file, second_file)
-    print(x)
-    result = json.dumps(json.loads(x))
-    # result = result.replace('"', '')
-    # result = result.replace(',', '')
+    result = get_line(first_file, second_file)
     return result
 
 
